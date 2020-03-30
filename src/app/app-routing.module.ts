89f81add43c41befbae4fa73025ad382b7185324
Nicolassaba33 <nicolassaba33@gmail.com>
2020-03-30 20:19:40 -0300

    usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { AuthGuard } from './guards/auth.guard';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'usuarios', component: UsuariosComponent,}, //canActivate: [AuthGuard] },
+  { path: 'usuario', component: UsuarioComponent,}//canActivate: [AuthGuard]}
+ 
+]
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyectF'`, () => {
+  it(`should have as title 'practico'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectF');
+    expect(app.title).toEqual('practico');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proyectF app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('practico app is running!');
   });
 });
